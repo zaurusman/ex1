6bd78bf70774b208cf4f2380eed6937fafb66a38
EladHillel <eladhillel6365@gmail.com>
2022-04-11 22:06:01 +0300

    Added AsciiArt files

---
+++ AsciiArt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elad on 11/04/2022.
+//
+
+#include "AsciiArt.h"
---
+++ AsciiArt.h
@@ -0,0 +1,8 @@
+//
+// Created by Elad on 11/04/2022.
+//
+
+#ifndef EX1_ASCIIART_H
+#define EX1_ASCIIART_H
+
+#endif //EX1_ASCIIART_H
